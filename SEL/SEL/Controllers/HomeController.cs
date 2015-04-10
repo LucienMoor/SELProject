1fd02efb0a82f6a97825a0015c825427e1755d98
LeaHuong <lea.demuralt@gmail.com>
2015-04-10 11:58:37 +0200

    added login and logout

--- SEL/SEL/Controllers/HomeController.cs
+++ SEL/SEL/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SEL.DAL;
+using SEL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace SEL.Controllers
 {
     public class HomeController : Controller
     {
+        private SelContext sel = new SelContext();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,29 @@ namespace SEL.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login(string password, string email)
+        {
+            User user = sel.User.Where(u => u.password == password && u.email == email).FirstOrDefault();
+            if(user == null)
+            {
+                ViewBag.Message = "Non valide Password or email";
+            }
+            else
+            {
+                ViewBag.Message = "You are logged in";
+                Session["login"] = user;
+            }
+            return View("Index");
+            
+        }
+
+        public ActionResult Logout()
+        {
+            Session["login"] = null;
+            return View("Index");
+
         }
     }
 }
